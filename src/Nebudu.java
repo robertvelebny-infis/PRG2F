91b73f7fef4de86af416be82ab2c92390cd86594
robertvelebny <robert.velebny@infis.cz>
2024-09-12 15:24:45 +0200

    Update Nebudu.java

--- src/Nebudu.java
+++ src/Nebudu.java
@@ -1,5 +1,5 @@
 public class Nebudu {
     public static void main(String[] args) {
-        System.out.println("zakaznik je jako mic. nesmi spadnout na zem.");
+        System.out.println("zakaznik je jako mic. nesmi spadnout na zem.   dsfsf");
     }
 }
